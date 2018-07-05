44d6109d75f30d4b0d1d6418cf6be7625639d777
Rafael Venera <rafael@revd.com.br>
2018-07-05 17:11:48 -0300

    Update trabalho_folium.py

--- Lógica de Programação/MAPS/trabalho_folium.py
+++ Lógica de Programação/MAPS/trabalho_folium.py
@@ -12,12 +12,6 @@ mapa = folium.Map(
     zoom_start=4)
 
 crime1 = MarkerCluster().add_to(mapa)
-crime2 = MarkerCluster().add_to(mapa)
-crime3 = MarkerCluster().add_to(mapa)
-crime4 = MarkerCluster().add_to(mapa)
-crime5 = MarkerCluster().add_to(mapa)
-crime6 = MarkerCluster().add_to(mapa)
-crime7 = MarkerCluster().add_to(mapa)
 
 csv = pd.read_csv('importar1.csv', encoding = 'latin-1', delimiter= ';')
 
@@ -26,7 +20,6 @@ crimes = csv[csv['CIDADE'] != '']
 for _, csv in crimes.iterrows():
 
     latlng = pegaLatLog(csv['CIDADE'] + ",SC")
-
    
     if csv['MV HOMICIDIO DOLOSO'] > 0:
         folium.Marker([latlng[0], latlng[1]],
